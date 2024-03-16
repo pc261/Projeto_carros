a7b3921b41c3661a55739281cce5f204db9c0681
unknown <santosfelipeliipe@gmail.com>
2024-03-16 13:06:00 -0300

    segundo commit do App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
+import iconSedans from "./assets/icon-sedans.svg"
+import iconSuvs from "./assets/icon-suvs.svg"
+import iconLuxury from "./assets/icon-luxury.svg"
+
 export default function App() {
   return(
-    <h1>Hello World</h1>
+    <main>
+      <section>
+
+        <div>
+          <img src={iconSedans} alt="" />
+          <h2>Sendans</h2>
+          <p>Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
+          <a href="https://www.webmotors.com.br/sedans" target="_blank">Ver mais</a>
+        </div>
+
+        <div>
+          <img src={iconSuvs} alt="" />
+          <h2>SUVs</h2>
+          <p>Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
+          <a href="https://www.webmotors.com.br/suvs"target="_blank" >Ver mais</a>
+        </div>
+
+        <div>
+          <img src={iconSuvs} alt="" />
+          <h2>Luxuosos</h2>
+          <p>Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
+          <a href="https://www.webmotors.com.br/carros-de-luxo/carros/estoque?estadocidade=estoque&necessidade=Carros%20de%20luxo&idcmpint=t1:c17:m07:portal-ofertas:busque-por-categorias::carros-de-luxo:comprar" target="_blank">Ver mais</a>
+        </div> 
+        
+      </section>
+    </main>
   )
 }
